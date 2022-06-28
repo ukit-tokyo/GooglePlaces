00f9d9c76506e777a1c55dc53899b6ea66cc034f
ukit-tokyo <t.yuki@ukit.tokyo>
2022-06-28 17:55:40 +0900

    Create Project

---
+++ GooglePlacesMock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GooglePlacesMock
+//
+//  Created by Taichi Yuki on 2022/06/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
