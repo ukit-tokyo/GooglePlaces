b76d7db55761d0378ff5c5f4ae57100af367e2cf
ukit-tokyo <t.yuki@ukit.tokyo>
2022-06-28 18:25:35 +0900

    Setup GooglePlaceAPI

--- GooglePlacesMock/AppDelegate.swift
+++ GooglePlacesMock/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import GooglePlaces
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +14,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
+
+    // Setup Google Places API
+    guard let apiKey = Bundle.main.object(forInfoDictionaryKey: "GOOGLE_PLACES_API_KEY") as? String, !apiKey.isEmpty else {
+      fatalError("GOOGLE_PLACES_API_KEY was not found.")
+    }
+
+    GMSPlacesClient.provideAPIKey(apiKey)
+
     return true
   }
 
