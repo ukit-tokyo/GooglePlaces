814afcc2413926b73526e2bb0fcfc24cd784cedd
ukit-tokyo <t.yuki@ukit.tokyo>
2022-06-28 19:30:10 +0900

    Get Current Place

--- GooglePlacesMock/ViewController.swift
+++ GooglePlacesMock/ViewController.swift
@@ -6,14 +6,43 @@
 //
 
 import UIKit
+import GooglePlaces
+import CoreLocation
 
 class ViewController: UIViewController {
 
+  private let client = GMSPlacesClient.shared()
+  private let locationManager = CLLocationManager()
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+
+    let status = CLLocationManager.authorizationStatus()
+    switch status {
+    case .authorizedAlways, .authorizedWhenInUse:
+      getCurrenPlace()
+    default:
+      locationManager.requestWhenInUseAuthorization()
+    }
   }
 
+  private func getCurrenPlace() {
+    let placeFields: GMSPlaceField = [.name, .formattedAddress]
+    client.findPlaceLikelihoodsFromCurrentLocation(withPlaceFields: placeFields) { (placeLikelihoods, error) in
 
+      guard error == nil else {
+        print("Current place error: \(error?.localizedDescription ?? "")")
+        return
+      }
+      guard let place = placeLikelihoods?.first?.place else {
+        print("No current place")
+        return
+      }
+
+      print("testing...", placeLikelihoods?.count)
+      print("testing...", place.name)
+      print("testing...", place.formattedAddress)
+    }
+  }
 }
 
